e533f94ce238aa50c273ecfd329c8dc315d48b30
faafzal <faafzal@outlook.com>
2016-01-14 15:42:07 -0600

    Create Model.java

---
+++ Model.java
@@ -0,0 +1,50 @@
+package org.pyte.userlogin;
+
+/**
+ * 
+ * This is the Model class. It has the database information. In this case,
+ * the database is a 2D array.
+ * 
+ * @author fafzal
+ *
+ */
+
+public class Model {
+	
+	//initialize databaseArray
+	private String[][] databaseArray;
+
+	/* 
+	 * create the 2D array in constructor so that it creates the database
+	 * when the Model object is created.
+	 */
+	public Model(){
+		
+		// The database array is a 10 x 2 array.
+		this.databaseArray = new String[10][2];
+		
+		//loops through the array to add values
+		for(int i = 0; i < this.databaseArray.length; i++){
+			
+			//this function adds 1 to i so that the array creates users from 1 to 10
+			int count = i+1;
+			
+			//databaseArray[i][0] will store the usernames
+			this.databaseArray[i][0] = "user" + count;
+			
+			//databaseArray[i][1] will store the passwords
+			this.databaseArray[i][1] = "pass" + count;
+		
+		}
+		
+	}
+	
+	//getter for the database array
+	public String[][] getDatabaseArray(){
+		
+		return this.databaseArray;
+		
+	}
+	
+	
+}
