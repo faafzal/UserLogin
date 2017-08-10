1546d494f3b0b569d289eb0f08614e2bc4e14463
faafzal <faafzal@outlook.com>
2017-08-10 15:55:02 -0700

    Remove Unnecessary Spacing

--- ModelTest.java
+++ ModelTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
  *
  */
 public class ModelTest {
-
 	//create a Model object
 	Model myModel = new Model();
 	
@@ -18,14 +17,11 @@ public class ModelTest {
 	 */
 	@Test
 	public void testDatabaseArray() {
-		
 		//assert to see if the database array has the correct values.
 		assertArrayEquals(myModel.getDatabaseArray(), new String[][]{{"user1", "pass1"},
 			{"user2", "pass2"}, {"user3", "pass3"}, {"user4", "pass4"}, {"user5", "pass5"},
 			{"user6", "pass6"}, {"user7", "pass7"},{"user8", "pass8"}, {"user9", "pass9"},
 			{"user10", "pass10"}
 		});
-		
 	}
-
 }
